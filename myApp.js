27625a1ed2d25b605311fb312a183fe16ac88c2a
Burhaann <burhaann321@gmail.com>
2023-11-28 11:55:15 +0530

    Use model.find() to Search Your Database

--- myApp.js
+++ myApp.js
@@ -49,7 +49,7 @@ console.log("------4");
 const createManyPeople = (arrayOfPeople, done) => {
   arrayOfPeople = [
     { name: "Basit", age: 28, favoriteFoods: ["rajma-dal", "pizza", "fish"] },
-    { name: "Moin", age: 27, favoriteFoods: ["chicken", "pizza", "meat"] },
+    { name: "Moin Khan", age: 27, favoriteFoods: ["chicken", "pizza", "meat"] },
     { name: "Arhaann", age: 26, favoriteFoods: ["meat", "chicken"] },
   ];
   console.log("------5");
